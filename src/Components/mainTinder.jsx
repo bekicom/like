8359c35479ca0d1aed5a920f8fbeb67baf25791b
jasurbekfrontend <j31548359@gmail.com>
2024-11-16 12:25:38 +0500

    m

--- src/Components/mainTinder.jsx
+++ src/Components/mainTinder.jsx
@@ -1,31 +1,92 @@
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-import './mainTinder.css';
-import img1 from '../img/swiper-img/11111111111111.png';
-import img2 from '../img/swiper-img/2222222222222222.png';
-import { EffectCards } from 'swiper/modules';
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import "./mainTinder.css";
+import img1 from "../img/swiper-img/11111111111111.png";
+import img2 from "../img/swiper-img/2222222222222222.png";
+import { EffectCards } from "swiper/modules";
 import { IoClose } from "react-icons/io5";
 import { FaSms } from "react-icons/fa";
-import Footer from './Footer';
-import Header from './Header';
+import Footer from "./Footer";
+import Header from "./Header";
 
 const TinderCards = () => {
   const girls = [
-    { id: 1, name: 'Sarina', img: img1 },
-    { id: 2, name: 'Malika', img: img2 },
-    { id: 3, name: 'Yasmina', img: img1 },
-    { id: 4, name: 'Zarina', img: img2 },
-    { id: 5, name: 'Sarina', img: img1 },
-    { id: 6, name: 'Malika', img: img2 },
-    { id: 7, name: 'Yasmina', img: img1 },
-
-
+    {
+      id: 1,
+      name: "Sarina",
+      img: img1,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
+    {
+      id: 2,
+      name: "Malika",
+      img: img2,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
+    {
+      id: 3,
+      name: "Yasmina",
+      img: img1,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
+    {
+      id: 4,
+      name: "Zarina",
+      img: img2,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
+    {
+      id: 5,
+      name: "Sarina",
+      img: img1,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
+    {
+      id: 6,
+      name: "Malika",
+      img: img2,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
+    {
+      id: 7,
+      name: "Yasmina",
+      img: img1,
+      age: 36,
+      city: "Москва",
+      status: "Свободная",
+      height: 166,
+      nationality: "Русский",
+    },
   ];
 
   const [swiperInstance, setSwiperInstance] = useState(null);
-  const [animationText, setAnimationText] = useState('');
+  const [animationText, setAnimationText] = useState("");
 
   // Slide o'zgarganda qaysi tomonga o'zgarganini aniqlash
   const handleSlideChange = (swiper) => {
@@ -34,15 +95,15 @@ const TinderCards = () => {
 
     if (currentIndex > previousIndex) {
       // O'ngga surilgan (right)
-      setAnimationText('x');
+      setAnimationText("x");
       setTimeout(() => {
-        setAnimationText('');
+        setAnimationText("");
       }, 500); // 0.5 sekunddan keyin matnni yo'qotish
     } else if (currentIndex < previousIndex) {
       // Chapga surilgan (left)
-      setAnimationText('❤');
+      setAnimationText("❤");
       setTimeout(() => {
-        setAnimationText('');
+        setAnimationText("");
       }, 500);
     }
   };
@@ -50,36 +111,55 @@ const TinderCards = () => {
     <div className="tinder-container">
       <Header />
 
-
       {animationText && (
-        <div className={`animation-text ${animationText === 'x' ? 'dislike' : 'like'}`}>
+        <div
+          className={`animation-text ${
+            animationText === "x" ? "dislike" : "like"
+          }`}
+        >
           {animationText}
         </div>
       )}
 
       <Swiper
-        effect={'cards'}
+        effect={"cards"}
         grabCursor={true}
         modules={[EffectCards]}
-        onSwiper={setSwiperInstance} // Swiper instance'ini saqlash
-        onSlideChange={handleSlideChange} // Slide o'zgarganda ishlaydigan funksiya
+        onSwiper={setSwiperInstance}
+        onSlideChange={handleSlideChange}
         className="mySwiper"
       >
         {girls.map((girl) => (
-          
           <SwiperSlide key={girl.id} className="girl-item">
-
-            <img id='img' src={girl.img} alt={girl.name} />
+            <img id="img" src={girl.img} alt={girl.name} />
             <div className="buttons-container">
-              <button className="dislike-button" onClick={() => swiperInstance?.slideNext()}>
+              <button
+                className="dislike-button"
+                onClick={() => swiperInstance?.slideNext()}
+              >
                 <IoClose />
               </button>
               <button className="dislike-sms">
-                <FaSms color='#AA3FEC' size={40} />
+                <FaSms color="#AA3FEC" size={40} />
               </button>
-              <button className="like-button" onClick={() => swiperInstance?.slidePrev()}>
+              <button
+                className="like-button"
+                onClick={() => swiperInstance?.slidePrev()}
+              >
                 ❤
               </button>
+            </div>
+            <div className="girl_info">
+              <b>
+                {girl.name} {girl.age}
+              </b>
+              <p>{girl.city}</p>
+              <p>{girl.status}</p>
+              <p>
+                {girl.height}
+                см рост
+              </p>
+              <p>{girl.nationality}</p>
             </div>
           </SwiperSlide>
         ))}
